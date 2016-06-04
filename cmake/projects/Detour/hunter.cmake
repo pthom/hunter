cc9f0026ada4a0361c67fbb6c599e4111dada845
David Hirvonen <dhirvonen@elucideye.com>
2016-06-04 16:20:32 -0400

    Update Detour package (initial release was empty)

--- cmake/projects/Detour/hunter.cmake
+++ cmake/projects/Detour/hunter.cmake
@@ -12,11 +12,11 @@ hunter_add_version(
     PACKAGE_NAME
     Detour
     VERSION
-    "1.0.0"
+    "1.0.0-p0"
     URL
-    "https://github.com/hunter-packages/Detour/archive/v1.0.0.tar.gz"
+    "https://github.com/hunter-packages/Detour/archive/v1.0.0-p0.tar.gz"
     SHA1
-    b64291c559587a90b489278cddea39459cf79c9d    
+    496011d5eab4367ef9e11c08b001445c8a24441c
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
